1d2ebde4957ae06d6116e32528be8367bb9dd4bc
lch <leadon@empal.com>
2019-08-16 23:55:19 +0900

    jk

--- vsCode/src/login/MapTest.java
+++ vsCode/src/login/MapTest.java
@@ -7,18 +7,22 @@ import java.util.Map;
 
 public class MapTest {
 	public List<Map<String, Object>> returnMap() {
-		Map<String, Object> map = new HashMap<String, Object>();
+		
 		List<Map<String, Object>> list = new ArrayList<>();
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+
 		map.put("user_id", "korea");
 		map.put("user_name", "kName");
 		map.put("user_pw", "12345");
 		list.add(map);
+		
 		map = new HashMap<String, Object>();
 		map.put("user_id", "japan");
 		map.put("user_name", "jName");
 		map.put("user_pw", "23456");
 		list.add(map);
-
+		
 		return list;
 
 	}
