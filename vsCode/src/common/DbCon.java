38959f79dbcc40fddad962c1e746b0c4d3ed8650
leadon <leadon@localhost>
2020-02-25 19:38:47 +0900

    jkl

--- vsCode/src/common/DbCon.java
+++ vsCode/src/common/DbCon.java
@@ -15,7 +15,7 @@ public class DbCon {
 	 */
 	static String jdbc_driver = "oracle.jdbc.driver.OracleDriver";
 	//static String jdbc_url = "jdbc:oracle:thin:@192.168.0.17:1521:orcl";
-	static String jdbc_url = "jdbc:oracle:thin:@localhost:1521:orcl";
+	static String jdbc_url = "jdbc:oracle:thin:@192.168.0.17:1521:orcl";
 
 	/* MySQL 연결정보 */
 	// String jdbc_driver = "com.mysql.jdbc.Driver";
@@ -26,7 +26,7 @@ public class DbCon {
 		try {
 			Class.forName(jdbc_driver);
 
-			conn = DriverManager.getConnection(jdbc_url, "hr", "hr");
+			conn = DriverManager.getConnection(jdbc_url, "lch", "lch");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
